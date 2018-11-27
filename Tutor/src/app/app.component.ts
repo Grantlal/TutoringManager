313fab8587e4bf8b1c40e6d9f9192f089f60b4bf
Grant <granttlalonde@gmail.com>
2018-11-27 10:28:25 -0600

    Updating Header

--- Tutor/src/app/app.component.ts
+++ Tutor/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tutor';
+  title = 'Next Gen Learning Management System';
 }
