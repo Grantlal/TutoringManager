ba243a15d923e69fd62a9efa1d9d1c668e2583b9
grant <granttlalonde@gmail.com>
2018-12-02 14:56:37 -0600

    Added todo list tutors available and due dates

--- ng-bootstrap/src/app/pages/home/home.component.ts
+++ ng-bootstrap/src/app/pages/home/home.component.ts
@@ -7,6 +7,20 @@ import {OnInit, Component} from "@angular/core";
 export class HomeComponent implements OnInit {
     constructor() { }
 
+    Checkoff(): void {
+        jQuery("body").toggleClass("m-l-xs todo-completed");
+    }
+
+    EmailTa(): void {
+        window.open("https://outlook.office.com/owa/");
+    }
+
+    EmailProf(): void {
+        window.open("https://outlook.office.com/owa/");
+    }
+    CheckoutTutor(): void {
+        window.open("https://tutor.stcloudstate.edu/TracWeb40/Default.html")
+    }
     ngOnInit() {
     }
 }
